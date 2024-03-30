1314cd60fe7c0ac9fed219162c85126efdb6ca31
email <bektowravshanov@gmail.com>
2024-03-30 11:05:03 +0500

    nurullo

--- home/urls.py
+++ home/urls.py
@@ -21,6 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
